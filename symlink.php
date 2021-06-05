f2dbefcdee747ae3fa2e5c22f5b176ac56f5f871
Haytam <hbakouane@gmail.com>
2021-06-05 03:03:50 +0100

    Removed* a file

--- symlink.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$targetFolder = $_SERVER['DOCUMENT_ROOT'].'storage/app/public';
-$linkFolder = $_SERVER['DOCUMENT_ROOT'].'public/storage';
-if (symlink($targetFolder,$linkFolder)) {
-    echo 'Symlink process successfully completed';
-}
